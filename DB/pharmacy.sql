b0d49096c2c5b95ca4f944f5381402571eeadc3b
Ovress <j4govress@gmail.com>
2023-12-26 23:16:10 +0200

    DB

--- DB/pharmacy.sql
+++ DB/pharmacy.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 21, 2023 at 07:36 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Dec 26, 2023 at 10:14 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
